f9b1d5aaf25944288cf804cdcecb1445c270ffe3
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-21 16:32:43 +0530

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
+import SubHeading from "../../components/SubHeading/SubHeading";
 
-import './Header.css';
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="section__padding app__header app__wrapper" id="Home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+        accusantium velit, sit perspiciatis doloribus nostrum distinctio tenetur
+        delectus repudiandae mollitia?
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header" />
+    </div>
   </div>
 );
 
