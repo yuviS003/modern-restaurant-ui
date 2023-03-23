c48e4d16ae88c9be29d62812076be39fc53265d3
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-23 17:24:34 +0530

    chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we beleive in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_qoute">
+          <img src={images.quote} alt="qoute" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur,
+            adipisci?
+          </p>
+        </div>
+        <p className="p__opensans">
+          Accusantium exercitationem ad ducimus eaque accusamus.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sing_language" />
+      </div>
+    </div>
   </div>
 );
 
