c18968d4704d70bfd1221c648ce61464e8525cb4
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-03-23 17:37:13 +0530

    intro

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import { meal } from "../../constants";
+import "./Intro.css";
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo((prev) => !prev);
+            playVideo ? vidRef.current.pause() : vidRef.current.play();
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
